ac995dc906156f255332cbaa39c3de7bcf62780e
shivashankarareddy <ssr2tech@gmail.com>
2024-04-07 20:17:49 +0530

    inprogress

--- App.js
+++ App.js
@@ -1,35 +1,66 @@
-import React from 'react';
-import  ReactDOM from 'react-dom/client';
-/* <div id= "parent"> 
-<div id= "child">
- <h1> hello<h1>
-</div>
-*</div>
-*
-*
-*at the end react element is an object only
-*/ 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement
-("div",
-{id:"parent"},
- [React.createElement(
-    "div",
-    {id:"child1"},
-  [React.createElement("h1",{},"hello word"),React.createElement("h2",{},"hello word")]
-  ),
-  React.createElement(
-    "div",
-    {id:"child2"},
-  [React.createElement("h1",{},"hello word"),React.createElement("h2",{},"hello word")]
-  )])//suppose i want 2 h1 or h2 tag means sibling then we need to warp with []
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <img className='header-img'  src="https://marketplace.canva.com/EAFauoQSZtY/1/0/1600w/canva-brown-mascot-lion-free-logo-qJptouniZ0A.jpg" alt="" />
+        <div className="header-menu">
+          <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+            <li>CART</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
-console.log(parent)
+const Cards=()=>{
 
-// its very dificulte to mainatin and more complex to read so that is why JSX came into the picture
+  return(
+    <>
+     <div className="res-card">
+      <img className="res-img" src="" alt=""/>
+      <h1>Meghana Foods</h1>
+      <h3>Biriyani</h3>
+      <h4>South,North style</h4>
+      <h4>4.5 Starts</h4>
+      <h4> Rs.150</h4>
 
-// const heading = React.createElement('h1', {
-//     id:"heading", xyz:"abc",  //will give atttribute
-// }, "hello shankar");
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+
+     </div>
+    </>
+  );
+}
+
+const Body = ()=>{
+  return(
+    <>
+    <div className="search">
+      <label>Search</label>
+      <input type="text" id="text" />
+        </div>
+        <div className="restoruants">
+          <Cards/>
+        </div>
+    </>
+  );
+
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body/>
+      
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
