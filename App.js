7957a59cba3ba22608542d03955b0198771a070c
shivashankarareddy <ssr2tech@gmail.com>
2024-04-08 09:35:18 +0530

    inprogress

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <div className="header">
-        <img className='header-img'  src="https://marketplace.canva.com/EAFauoQSZtY/1/0/1600w/canva-brown-mascot-lion-free-logo-qJptouniZ0A.jpg" alt="" />
+        <img className='header-img'  src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg" alt="" />
         <div className="header-menu">
           <ul>
             <li>HOME</li>
@@ -19,19 +19,17 @@ const Header = () => {
   );
 };
 
-const Cards=()=>{
+const Cards=(props)=>{
 
   return(
     <>
      <div className="res-card">
-      <img className="res-img" src="" alt=""/>
+      <img className="res-img" src="https://png.pngtree.com/png-clipart/20230522/original/pngtree-chicken-biryani-front-view-png-image_9167532.png" alt=""/>
       <h1>Meghana Foods</h1>
       <h3>Biriyani</h3>
       <h4>South,North style</h4>
       <h4>4.5 Starts</h4>
       <h4> Rs.150</h4>
-
-
      </div>
     </>
   );
@@ -43,8 +41,13 @@ const Body = ()=>{
     <div className="search">
       <label>Search</label>
       <input type="text" id="text" />
+      <button>Search</button>
         </div>
         <div className="restoruants">
+          <Cards/>
+          <Cards resName="KFC"/>
+          <Cards/>
+          <Cards/>
           <Cards/>
         </div>
     </>
