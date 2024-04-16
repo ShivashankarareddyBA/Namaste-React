5e063f49f9c827057878e6d8076f434ed821baf0
shivashankarareddy <ssr2tech@gmail.com>
2024-04-16 11:44:12 +0530

    inprogress..

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div> 
-        <h1>Hello</h1>
+    <div className='about'> 
+        <h2>Shiva</h2>
+        <h2>2 Years of</h2>
+        <h2> Experienced </h2>
+        <h2>frontend developer </h2>
+        <h2>Karnataka</h2>
       
     </div>
   )
--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      <h1>88899</h1>
+    <div className='contact'>
+      <h2>Shiva</h2>
+        <h2>Mobile +91 9916838182</h2>
+        <h2>Email ssr2tech@gmail.com</h2>
+        <h2>Karnataka</h2>
     </div>
   )
 }
