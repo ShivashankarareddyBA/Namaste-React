c63c1ee9760139e37b883afa66325866193f0c64
shivashankarareddy <ssr2tech@gmail.com>
2024-04-19 12:02:14 +0530

    update

--- src/Component/RestaurantMenu.jsx
+++ src/Component/RestaurantMenu.jsx
@@ -2,24 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_URL } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+    const { resId } = useParams();
 
-  const { resId } = useParams();
+    const resInfo = useRestaurantMenu(resId);
 
-  useEffect(() => {
-    fethMenu();
-  }, []);
-
-  const fethMenu = async () => {
-    const data = await fetch(MENU_URL + resId);
-    const json = await data.json();
-    setResInfo(json.data);
-  };
-
-  const cardInfo = resInfo?.cards[2]?.card?.card?.info;
+   const cardInfo = resInfo?.cards[2]?.card?.card?.info;
   const { name, cuisines, costForTwoMessage } = cardInfo || {};
 
   const cardGroup =
