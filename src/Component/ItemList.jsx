34a5873416106c4a2029bda41a39e5c17fcf8911
shivashankarareddy <ssr2tech@gmail.com>
2024-06-01 20:54:45 +0530

    itemList pages image has updated with fixed size

--- src/Component/ItemList.jsx
+++ src/Component/ItemList.jsx
@@ -1,47 +1,103 @@
+// import { useDispatch } from "react-redux";
+// import {IMG} from "../utils/constants"
+// import { addItem } from "../utils/cartSlice";
+
+// const ItemList = ({ items }) => {
+  
+//   const dispatch = useDispatch();
+//   const handleAddItem = (item)=>{
+//     dispatch(addItem(item))
+    
+//   }
+
+//   return (
+//     <div>
+//       {items.map((item) => (
+//         <div
+//           key={item.card.info.id}
+//           className=" flex justify-between p-2 m-2  border-gray-200 border-b-2 text-left"
+//         >
+           
+//           <div className="w-9\12">
+//           <div className="py-2">
+//             <span>{item.card.info.name} </span>
+//             <span>
+//               - ₹
+//               {item.card.info.price
+//                 ? item.card.info.price / 100
+//                 : (item.card.info.finalPrice / 100) |
+//                   (item.card.info.defaultPrice / 100)}
+//             </span>
+//           </div>
+//           <p className="text-xs"> {item.card.info.description}</p>
+        
+//           </div>
+//           <div className="w-3/12 p-4 mx-auto" >
+//             <div className="absolute"> 
+//             <button className=" p-2 mx-4 my-10 rounded-lg bg-slate-200 text-black shadow-lg"
+//             onClick={()=> handleAddItem(item)}> Add +</button>
+//             </div>
+//             <img src={IMG + item.card.info.imageId}  className="w-32 h-32 object-cover"/>
+//             </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+// export default ItemList;
+
+
+
 import { useDispatch } from "react-redux";
-import {IMG} from "../utils/constants"
+import { IMG } from "../utils/constants";
 import { addItem } from "../utils/cartSlice";
 
 const ItemList = ({ items }) => {
-  
   const dispatch = useDispatch();
-  const handleAddItem = (item)=>{
-    dispatch(addItem(item))
-    
-  }
+
+  const handleAddItem = (item) => {
+    dispatch(addItem(item));
+  };
 
   return (
     <div>
       {items.map((item) => (
         <div
           key={item.card.info.id}
-          className=" flex justify-between p-2 m-2  border-gray-200 border-b-2 text-left"
+          className="flex justify-between p-2 m-2 border-gray-200 border-b-2 text-left"
         >
-           
-          <div className="w-9\12">
-          <div className="py-2">
-            <span>{item.card.info.name} </span>
-            <span>
-              - ₹
-              {item.card.info.price
-                ? item.card.info.price / 100
-                : (item.card.info.finalPrice / 100) |
-                  (item.card.info.defaultPrice / 100)}
-            </span>
-          </div>
-          <p className="text-xs"> {item.card.info.description}</p>
-        
-          </div>
-          <div className="w-3/12 p-4" >
-            <div className="absolute"> 
-            <button className=" p-2 mx-4 my-10 rounded-lg bg-slate-200 text-black shadow-lg"
-            onClick={()=> handleAddItem(item)}> Add +</button>
+          <div className="w-9/12">
+            <div className="py-2">
+              <span>{item.card.info.name}</span>
+              <span>
+                - ₹
+                {item.card.info.price
+                  ? item.card.info.price / 100
+                  : item.card.info.finalPrice / 100 || item.card.info.defaultPrice / 100}
+              </span>
             </div>
-            <img src={IMG + item.card.info.imageId}  className="w-full"/>
+            <p className="text-xs">{item.card.info.description}</p>
+          </div>
+          <div className="w-3/12 p-4 mx-auto relative">
+            <div className="absolute top-0 right-0">
+              <button
+                className="p-2 mx-4 my-2 rounded-lg bg-slate-200 text-black shadow-lg"
+                onClick={() => handleAddItem(item)}
+              >
+                Add +
+              </button>
             </div>
+            <img
+              src={IMG + item.card.info.imageId}
+              className="w-32 h-32 object-cover"
+              alt={item.card.info.name}
+            />
+          </div>
         </div>
       ))}
     </div>
   );
 };
+
 export default ItemList;
+
