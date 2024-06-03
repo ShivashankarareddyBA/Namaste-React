48b4488405e192c8d444d238974d502e4d764d75
shivashankarareddy <ssr2tech@gmail.com>
2024-06-03 07:58:42 +0530

    Header Test file update

---
+++ src/Component/__tests__/Header.test.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Header from "../Header";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import '@testing-library/jest-dom'; 
+import appStore from "../../utils/appStore"
+
+it("should load Header component with a Login Button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  // Add assertions to check if the Login Button is in the document
+});
